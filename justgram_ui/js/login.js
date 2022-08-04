8c179dfc17821a88dd965b6c2c9dbaa243653a4e
miogy <jjangrl87@gmail.com>
2022-08-04 19:29:52 +0900

    mission2수정

--- justgram_ui/js/login.js
+++ justgram_ui/js/login.js
@@ -1,15 +1,32 @@
-const idForm = document.querySelector('#userId');
-const pwForm = document.querySelector('#userPw');
-const loginBtn = document.querySelector('#loginBtn');
+//3가지 요소가 필요함       elements:배열안으로 들어감
+const idForm = document.getElementById('idForm');  //id요소
+const pwForm = document.getElementById('pwForm');  //pw요소
+const btnEvent = document.getElementById('btn');   //btn요소
 
-idForm.addEventListener('keyup',activeEvent);
-pwForm.addEventListener('keyup',activeEvent);
-loginBtn.addEventListener('click',errorEvent);
+function onInput(){
+  // 브라우저 콘솔창에 event.target : 해당이벤트를 실행한 요소가 보임
+  const id = idForm.value;
+  const pw = pwForm.value;
 
-function activeEvent(){
-  switch(!(idForm.value && pwForm.value)){
-    case true : loginBtn.disabled = true; break;
-    case false : loginBtn.disabled = false; break
+  if(id.length >= 1 && pw.length >= 1) {
+    // 활성화
+    console.log('활성화')
+    btnEvent.style.backgroundColor = "#1e90ff";
+    btnEvent.style.cursor = "pointer";
+    btnEvent.disabled = false;
+  }else{
+    // 비활성화
+    console.log('비활성화');
+    btnEvent.style.backgroundColor = "#abd5ff";
+    btnEvent.style.cursor = "not-allowed";
+    btnEvent.disabled = true;
   }
 }
 
+idForm.addEventListener('input',onInput);
+pwForm.addEventListener('input',onInput);
+btnEvent.addEventListener('button', (event) => {
+  event.preventDefault();
+});
+
+onInput()
--- justgram_ui/js/main.js
+++ justgram_ui/js/main.js
@@ -1,2 +1,10 @@
-let inputComment = document.querySelector('inputNewComment');
-let 
+const inputComment = document.getElementsByClassName('inputNewComment');
+const commentBtn = document.getElementsByClassName('newCommentBtn');
+const commentList = document.getElementsByClassName('feedCommentlist');
+
+commentBtn.addEventListener('click', () => {
+  const content = inputComment.value;
+  
+
+  commentList  
+})
