27fa6ee0093947f7e4ac5223aa00d7b10499f119
miogy <jjangrl87@gmail.com>
2022-08-05 12:08:56 +0900

    mission4-end

--- justgram_ui/js/main.js
+++ justgram_ui/js/main.js
@@ -1,10 +1,40 @@
-const inputComment = document.getElementsByClassName('inputNewComment');
-const commentBtn = document.getElementsByClassName('newCommentBtn');
-const commentList = document.getElementsByClassName('feedCommentlist');
+// input comment
+const commentInput = document.getElementsByClassName("inputNewComment");
+// btn
+const commentBtn = document.getElementsByClassName("feedCommentBtn");
+// comment list
+const commentList = document.getElementsByClassName("feedCommentList");
 
-commentBtn.addEventListener('click', () => {
-  const content = inputComment.value;
+Array.from(commentBtn).forEach((btn, index) => {
+
+  btn.addEventListener('click', () => {
+    const content = commentInput[index].value;
+
+    const childElem = document.createElement('div');
+
+    const nameSpan = document.createElement('span');
+    nameSpan.className = 'comment-name';
+    nameSpan.textContent = 'miogy';
+
+    childElem.append(nameSpan, content);
+    commentList[index].appendChild(childElem);
+  });
+});
+
+//   //btn eventlistener
+//   commentBtn.addEventListener('click', () => {
+
+//   const content = commentInput.value;
+//   console.log(content)
+
+//   const childElem = document.createElement('div');
+
+//   const nameSpan = document.createElement('span');
+//   nameSpan.className = 'comment-name';
+//   nameSpan.textContent = 'miogy';
+
+//   childElem.append(nameSpan, content);
+//   commentList.appendChild(childElem);
+// });
   
 
-  commentList  
-})
