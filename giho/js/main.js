28e32441df77e6055aa5b8ada17ec1ce39092867
GPontheG <gpontheg@gmail.com>
2022-08-06 16:39:58 +0900

    Add: Mission 5

--- giho/js/main.js
+++ giho/js/main.js
@@ -2,7 +2,23 @@ const commentInput = document.getElementsByClassName("comment-write-input");
 const commentButton = document.getElementsByClassName("comment-write-button");
 const commentList = document.getElementsByClassName("feed-comment-list");
 
-console.log(Array.from(commentButton));
+fetch("./data/comments.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const commentArray = data.comments;
+    commentArray.forEach((comment) => {
+      for (let i = 0; i < commentArray.length; i++) {
+        const commentElem = document.createElement("div");
+        const nameSpan = document.createElement("span");
+        nameSpan.className = "comment-name";
+        nameSpan.textContent = comment.userName;
+        const content = comment.content;
+        commentElem.append(nameSpan, content);
+        commentList[i].append(commentElem);
+      }
+    });
+  });
+
 Array.from(commentButton).forEach((button, index) => {
   button.addEventListener("click", () => {
     const content = commentInput[index].value;
@@ -11,7 +27,7 @@ Array.from(commentButton).forEach((button, index) => {
 
     const nameSpan = document.createElement("span");
     nameSpan.className = "comment-name";
-    nameSpan.textContent = "GHI";
+    nameSpan.textContent = "ABC";
 
     childElem.append(nameSpan, content);
 
